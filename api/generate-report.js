7548ca8ae520f255f739d80478c5cb777e855a25
King L C <ckl.career@gmail.com>
2026-02-21 23:21:27 +0100

    Update generate-report.js

--- api/generate-report.js
+++ api/generate-report.js
@@ -8,6 +8,7 @@ return res.status(405).json({ error: 'Method not allowed' });
 try {
 const data = req.body;
 const html = generateReportHTML(data);
+return res.status(200).send(html);
 
 } catch (error) {
 return res.status(500).json({ error: error.message });
